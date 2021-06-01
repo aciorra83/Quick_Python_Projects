00fd4108090f830af640ae81cdae3a0d0422f72b
aciorra83 <alex.ciorra@gmail.com>
2021-06-01 09:58:07 -0400

    updated comments

--- renaminng_bulk_files.py
+++ renaminng_bulk_files.py
@@ -7,10 +7,10 @@ def main():
     path = '/home/alex/random_images/'
     # creating a for loop to iterate through the dir
     for filename in os.listdir(path):
-        my_dir = 'img' + str(i) + '.jpg'
+        my_img = 'img' + str(i) + '.jpg'
         my_source = path + filename
-        my_dir = path + my_dir
-        os.rename(my_source, my_dir)
+        my_img = path + my_img
+        os.rename(my_source, my_img)
         i += 1
 if __name__ == '__main__': 
     main()
