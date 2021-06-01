084addda0fe0334a303030dd1c927c762817e017
aciorra83 <alex.ciorra@gmail.com>
2021-06-01 11:12:49 -0400

    updated comments

--- weather_info.py
+++ weather_info.py
@@ -7,8 +7,10 @@ API_key = '67647fab6c0de384c7813d151913d53a'
 city = input('Enter a city: ')
 
 # where we send the response to
-# 
+# adding '?appid=' allows concatonation of the API_key plus a '&q=' or query to search for the user specified city
 base_url = 'https://api.openweathermap.org/data/2.5/weather?appid='+API_key+'&q='+city
+
+# storing the data from the base_url query 
 weather_data = requests.get(base_url).json()
 
 pprint(weather_data)
